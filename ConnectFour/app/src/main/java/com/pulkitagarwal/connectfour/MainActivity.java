e46d2d3d78545d3a1240617a4259585cdc12e390
Pulkit <pulkit.agarwal@sjsu.edu>
2019-11-14 12:57:35 -0800

    Connect Four Game

---
+++ ConnectFour/app/src/main/java/com/pulkitagarwal/connectfour/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pulkitagarwal.connectfour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
